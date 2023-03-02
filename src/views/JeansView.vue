5389d91597dbc4d72e5017efabe3075731c9f78c
Zekosz <dani.winberg@gmail.com>
2023-03-02 13:08:36 +0100

    4 kategorier man

---
+++ src/views/JeansView.vue
@@ -0,0 +1,103 @@
+<template>
+  <h1 class="text-center">Jeans</h1>
+  <b-container class="bv-example-row bv-example-row-flex-cols">
+    <b-row align-v="stretch">
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/84e7ff59-bbd8-4856-a672-faf04125d58b_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/d7eb83e7-3d58-440d-898f-79a2f26c0d20_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/873e1694-ccfa-4e72-97b3-2048cf61c450_large.jpg"
+          fluid
+          alt="Fluid image"
+        />
+      </b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/e8c25888-9068-407d-b594-d764b35ff06c_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+    </b-row>
+    <b-row class="text-center" align-v="stretch">
+      <b-col
+        ><p>Vintage G-Star Raw 3301</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Wrangler 90's Denim Straight Leg</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Levi's 514's Black Denim Jeans</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Calvin Klein 90's Denim</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+    </b-row>
+    <b-row align-v="stretch">
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/38b85db1-f713-4d3a-85d4-25d42497cbd2_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/03f5dca0-6a66-45ac-b73e-91be447d1ea0_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/90c7277d-6d7d-4e43-85e4-01dbdd480d7c_large.jpg"
+          fluid
+          alt="Fluid image"
+        />
+      </b-col>
+      <b-col
+        ><b-img
+          src="https://marketplace-images.asos.com/user/b45bd446-033d-4f7a-8c34-4072672fec0b_large.jpg"
+          fluid
+          alt="Fluid image"
+      /></b-col>
+    </b-row>
+    <b-row class="text-center" align-v="stretch">
+      <b-col
+        ><p>Levi's 90's 501 Denim Slim</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Vintage 90's Wrangler Jeans</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Vintage 90's Levi's 514 Denim</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+      <b-col
+        ><p>Vintage Carhartt Straight Fit Jeans</p>
+        <p>100 kr</p>
+        <a href="#" class="btn btn-primary">Add to Cart</a></b-col
+      >
+    </b-row>
+  </b-container>
+</template>
