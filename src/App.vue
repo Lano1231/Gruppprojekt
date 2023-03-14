3b4232f9f2fd76d7f9905c12a5bc314e5052ba32
KevinITHS <kevin.ali@iths.se>
2023-03-14 12:48:12 +0100

    Revert "mobile"
    This reverts commit 821154f21f272c82a7b5a58bed0b0c01b7695ff0.

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,7 @@
   export default {
     data: () => {
       return {
-        mobileView: true,
-        showNav: false
+        mobileView: true
       }
     },
     components: {
@@ -17,7 +16,7 @@
 </script>
 
 <template>
-  <HelloWorld v-if="!mobileView" />
+  <HelloWorld />
 
   <main>
     <RouterView />
@@ -26,56 +25,4 @@
   <ShowName />
 
   <LandPage v-if="!mobileView" />
-
-  <div class="app">
-    <Sidebar />
-    <router-view />
-  </div>
 </template>
-
-<script setup>
-  import Sidebar from './components/MobileNav.vue'
-</script>
-
-<style lang="scss">
-  :root {
-    --gray: #464646;
-    --dark-blue: #073c83;
-    --maincolor: #00ffdd;
-    --ljus: #f1e4e4f8;
-    --sidebar-width: 230px;
-    --primary-alt: #0a665a;
-    --dark-alt: #20201e;
-  }
-
-  * {
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-    font-family: 'Gill Sans', sans-serif;
-  }
-
-  body {
-    background: var(--light);
-  }
-
-  button {
-    cursor: pointer;
-    appearance: none;
-    border: none;
-    outline: none;
-    background: none;
-  }
-
-  .app {
-    display: flex;
-    main {
-      flex: 1 1 0;
-      padding: 2rem;
-
-      @media (max-width: 768px) {
-        padding-left: 6rem;
-      }
-    }
-  }
-</style>
