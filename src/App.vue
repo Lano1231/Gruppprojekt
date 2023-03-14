821154f21f272c82a7b5a58bed0b0c01b7695ff0
KevinITHS <kevin.ali@iths.se>
2023-03-14 12:33:10 +0100

    mobile

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,8 @@
   export default {
     data: () => {
       return {
-        mobileView: true
+        mobileView: true,
+        showNav: false
       }
     },
     components: {
@@ -19,7 +20,7 @@
 </script>
 
 <template>
-  <HelloWorld />
+  <HelloWorld v-if="!mobileView" />
 
   <main>
     <RouterView />
@@ -28,4 +29,56 @@
   <ShowName />
 
   <LandPage v-if="!mobileView" />
+
+  <div class="app">
+    <Sidebar />
+    <router-view />
+  </div>
 </template>
+
+<script setup>
+  import Sidebar from './components/MobileNav.vue'
+</script>
+
+<style lang="scss">
+  :root {
+    --gray: #464646;
+    --dark-blue: #073c83;
+    --maincolor: #00ffdd;
+    --ljus: #f1e4e4f8;
+    --sidebar-width: 230px;
+    --primary-alt: #0a665a;
+    --dark-alt: #20201e;
+  }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Gill Sans', sans-serif;
+  }
+
+  body {
+    background: var(--light);
+  }
+
+  button {
+    cursor: pointer;
+    appearance: none;
+    border: none;
+    outline: none;
+    background: none;
+  }
+
+  .app {
+    display: flex;
+    main {
+      flex: 1 1 0;
+      padding: 2rem;
+
+      @media (max-width: 768px) {
+        padding-left: 6rem;
+      }
+    }
+  }
+</style>
